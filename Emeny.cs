33067b0e5418863def31569083430a5e127b80b4
MichaelDiflorio <michael_5219@hotmail.com>
2016-11-05 15:57:29 -0400

    Add files via upload

---
+++ Emeny.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WebApp1
+{
+    class Emeny
+    {public class Enemy : Sprites
+        {
+            protected float startHealth;
+            protected float currentHealth;
+
+            protected bool alive = true;
+
+            protected float speed = 0.5f;
+            protected int bountyGiven;
+
+            public float CurrentHealth
+            {
+                get { return currentHealth; }
+                set { currentHealth = value; }
+            }
+
+            public bool IsDead
+            {
+                get { return currentHealth <= 0; }
+            }
+
+            public int BountyGiven
+            {
+                get { return bountyGiven; }
+            }
+
+            public Enemy(Texture2D texture, Vector2 position, float health, int bountyGiven, float speed)
+    : base(texture, position)
+            {
+                this.startHealth = health;
+                this.currentHealth = startHealth;
+                this.bountyGiven = bountyGiven;
+                this.speed = speed;
+            }
+            public override void Update(GameTime gameTime)
+            {
+                base.Update(gameTime);
+
+                if (currentHealth <= 0)
+                    alive = false;
+            }
+
+
+        } 
+    }
+}
---
+++ Sprites.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WebApp1
+{
+    class Sprites
+    {
+        protected Texture2D texture;
+
+        protected Vector2 position;
+        protected Vector2 velocity;
+
+        protected Vector2 center;
+        protected Vector2 origin;
+
+        protected float rotation;
+
+        public Vector2 Center
+        {
+            get { return center; }
+        }
+        public Sprites(Texture2D tex, Vector2 pos)
+        {
+             texture = tex;
+
+            position = pos;
+            velocity = Vector2.Zero;
+
+            center = new Vector2(position.X + texture.Width /
+                2, position.Y + texture.Height / 2);
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+            this.center = new Vector2(position.X + texture.Width / 2,
+            position.Y + texture.Height / 2);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, center, null, Color.White, rotation, origin, 1.0f, SpriteEffects.None, 0);
+        }
+    }
+}
