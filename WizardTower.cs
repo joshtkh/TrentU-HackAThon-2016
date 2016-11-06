bd0f2c7549138064d8932067a7b62360e9c98fc8
MichaelDiflorio <michael_5219@hotmail.com>
2016-11-06 05:05:36 -0500

    Add files via upload

---
+++ WizardTower.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp1
+{
+    public class WizardTower : Tower
+    {
+        public WizardTower(Texture2D texture, Texture2D ammoTexture, Vector2 position)
+            : base(texture, ammoTexture, position)
+        {//damage and stuff for wizards
+            this.damage = 25;
+
+            this.cost = 30;
+
+            this.radius = 150;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (bulletTimer >= 0.75f && target != null)
+            {
+                Bullet bullet = new Bullet(bulletTexture, Vector2.Subtract(center,
+                    new Vector2(ammoTexture.Width / 2)), rotation, 6, damage);
+
+                bulletList.Add(bullet);
+                bulletTimer = 0;
+            }
+
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                bullet bullet = bulletList[i];
+
+                bullet.SetRotation(rotation);
+                bullet.Update(gameTime);
+
+                if (!IsInRange(bullet.Center))
+                    bullet.Kill();
+
+                if (target != null && Vector2.Distance(bullet.Center, target.Center) < 15)
+                {
+                    target.CurrentHealth -= bullet.Damage;
+                    bullet.Kill();
+                }
+
+                if (bullet.IsDead())
+                {
+                    bulletList.Remove(bullet);
+                    i--;
+                }
+            }
+        }
+    }
+}
+
